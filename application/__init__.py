e3d9dc9f70bac4ea3eb0138a7b8faabf5c112b0a
ShengLu <sxlfhmsl@hotmail.com>
2019-06-30 17:52:02 +0800

    添加数据库迁移脚本命令记录

--- application/__init__.py
+++ application/__init__.py
@@ -11,20 +11,6 @@ from .dao.utils import db, flask_redis
 pymysql.install_as_MySQLdb()
 
 
-# 增加日志模块
-def setup_log(config):
-    # 设置日志等级
-    logging.basicConfig(level=config.LOG_LEVEL)
-    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
-    file_log_handler = RotatingFileHandler('log/log', maxBytes=1024 * 1024 * 300, backupCount=10)
-    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s')
-    # 为刚创建的日志记录器设置日志记录格式
-    file_log_handler.setFormatter(formatter)
-    # 为全局的日志工具对象（flask app使用的）添加日志记录器
-    logging.getLogger().addHandler(file_log_handler)
-
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -58,3 +44,16 @@ def create_app(test_config=None):
         return '<html>Hello, World!</html>'
     return app
 
+
+# 增加日志模块
+def setup_log(config):
+    # 设置日志等级
+    logging.basicConfig(level=config.LOG_LEVEL)
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler('log/log', maxBytes=1024 * 1024 * 300, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
--- readme.txt
+++ readme.txt
@@ -1,2 +1,14 @@
 pip freeze > requirements.txt		生成requirements
 pip install -r requirements.txt		安装requirements.txt依赖
+
+1. 初始化数据迁移的目录
+-m flask db init
+​
+2. 数据库的数据迁移版本初始化
+-m flask db migrate -m 'initial migration'
+​
+3. 升级版本[创建表]
+-m flask db upgrade
+​
+4. 降级版本[删除表]
+-m flask db  downgrade
