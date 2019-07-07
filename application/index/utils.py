c26d6902f03909e2e355ecaf559c35aa391a6c9f
ShengLu <sxlfhmsl@hotmail.com>
2019-07-07 23:20:40 +0800

    1.加入token检测

--- application/index/utils.py
+++ application/index/utils.py
@@ -2,8 +2,29 @@
 """
 定义一些基础东西
 """
+import functools
 from flask.views import View
-from flask import abort
+from flask import abort, request
+
+from ..dao.utils import RedisOp
+from ..service.auth import decode_auth_token
+
+
+def check_auth(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        auth = request.headers.get('auth', None)
+        token = RedisOp().get_normal(auth)
+        # 检测不到token
+        if token is None:
+            abort(404)
+        try:
+            token_dict = decode_auth_token(token)
+        except Exception as e:
+            print(e)
+            abort(404)
+        return fn(token_dict, *args, **kwargs)
+    return wrapper
 
 
 class BaseView(View):
