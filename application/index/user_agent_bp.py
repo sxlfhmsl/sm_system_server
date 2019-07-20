4195e62afe634bfeb21954e8687ba59ef6449351
ShengLu <sxlfhmsl@hotmail.com>
2019-07-21 02:24:39 +0800

    通过id更新代理

--- application/index/user_agent_bp.py
+++ application/index/user_agent_bp.py
@@ -8,6 +8,7 @@ from flask import jsonify, abort, current_app, request
 
 from .utils import PermissionView
 from .sta_code import SUCCESS, USER_SAME_LOGIN_NAME, OTHER_ERROR, POST_PARA_ERROR, USER_AGENT_LEVEL_LOW, QUERY_NO_RESULT
+from .sta_code import USER_AGENT_NOT_ENOUGH_MEMBER
 from ..service.user_agent_service import SmUserAgentService
 
 user_agent_bp = Blueprint('user/agent', __name__)
@@ -42,6 +43,8 @@ class CreateAgent(PermissionView):
                 return jsonify(USER_AGENT_LEVEL_LOW)
             elif result == 3:                                                      # 其他错误
                 return jsonify(OTHER_ERROR)
+            elif result == 4:                                                      # 可创建会员数量不足
+                return jsonify(USER_AGENT_NOT_ENOUGH_MEMBER)
         except Exception as e:
             current_app.logger.error(e)
             return jsonify(POST_PARA_ERROR)
@@ -55,7 +58,7 @@ class QueryAllAgent(PermissionView):
 
     def response_admin(self):
         try:
-            code, data = SmUserAgentService.query_agent(agent_id=None, **request.json)
+            code, data = SmUserAgentService.admin_query_agent(**request.json)
             if code == 0:
                 return jsonify(SUCCESS(data))
             elif code == 1:
@@ -67,7 +70,7 @@ class QueryAllAgent(PermissionView):
 
     def response_agent(self):
         try:
-            code, data = SmUserAgentService.query_agent(agent_id=self.u_id, **request.json)
+            code, data = SmUserAgentService.agent_query_agent(self.user, **request.json)
             if code == 0:
                 return jsonify(SUCCESS(data))
             elif code == 1:
@@ -98,11 +101,72 @@ class QueryAgentByID(PermissionView):
             return jsonify(QUERY_NO_RESULT)
 
     def response_agent(self):
-        result = SmUserAgentService.get_by_id(self.agent_id, self.u_id)
+        result = SmUserAgentService.agent_query_by_id(self.user, self.agent_id)
         if result:
             return jsonify(SUCCESS(result))
         else:
             return jsonify(QUERY_NO_RESULT)
+
+
+class ChangeAgentByID(PermissionView):
+    """
+    通过id更新代理信息， 管理员可以任意修改，代理只可修改自身次级代理
+    """
+
+    def __init__(self):
+        super(ChangeAgentByID, self).__init__()
+        self.agent_id = None    # 带查询的代理id
+
+    def dispatch_request(self, token_dict: dict, agent_id):
+        self.agent_id = agent_id
+        return super(ChangeAgentByID, self).dispatch_request(token_dict)
+
+    @staticmethod
+    def unpack_para(params: dict):
+        """
+        解析出欲修改数据
+        :param params: 所有参数
+        :return:
+        """
+        para_legal_list = ['LoginName', 'NickName', 'Margin', 'TestMargin', 'CommissionRatio', 'ExchangeRate',
+                           'MemberPrefix', 'MemberMaximum', 'Bank', 'BankAccount', 'Cardholder']
+        return {key: params[key] for key in params.keys() if key in para_legal_list}
+
+    def response_admin(self):
+        result = 0
+        try:
+            result = SmUserAgentService.update_by_id(self.agent_id, **self.unpack_para(request.json))
+        except Exception as e:
+            result = 3
+            current_app.logger.error(e)
+        if result == 0:
+            return jsonify(SUCCESS())
+        elif result == 1:
+            return jsonify(USER_SAME_LOGIN_NAME)
+        elif result == 2:
+            return jsonify(USER_AGENT_NOT_ENOUGH_MEMBER)
+        elif result == 3:
+            return jsonify(POST_PARA_ERROR)
+        return jsonify(OTHER_ERROR)
+
+    def response_agent(self):
+        result = 0
+        try:
+            result = SmUserAgentService.agent_update_by_id(self.user, **self.unpack_para(request.json))
+        except Exception as e:
+            result = 4
+            current_app.logger.error(e)
+        if result == 0:
+            return jsonify(SUCCESS())
+        elif result == 1:
+            return jsonify(USER_SAME_LOGIN_NAME)
+        elif result == 2:
+            return jsonify(USER_AGENT_LEVEL_LOW)
+        elif result == 3:
+            return jsonify(USER_AGENT_NOT_ENOUGH_MEMBER)
+        elif result == 4:
+            return jsonify(POST_PARA_ERROR)
+        return jsonify(OTHER_ERROR)
 
 
 # 创建代理
@@ -111,4 +175,5 @@ user_agent_bp.add_url_rule('/create', methods=['POST'], view_func=CreateAgent.as
 user_agent_bp.add_url_rule('/all', methods=['POST'], view_func=QueryAllAgent.as_view('all_agent'))
 # 查询单个代理，通过id
 user_agent_bp.add_url_rule('/query/<agent_id>', methods=['POST'], view_func=QueryAgentByID.as_view('query_agent_by_id'))
-
+# 通过id修改代理
+user_agent_bp.add_url_rule('/update/<agent_id>', methods=['POST'], view_func=ChangeAgentByID.as_view('update_agent_by_id'))
--- application/service/user_agent_service.py
+++ application/service/user_agent_service.py
@@ -70,6 +70,7 @@ class SmUserAgentService(BaseService):
                  1                   存在相同的用户名
                  2                   代理的等级过低
                  3                   其他错误
+                 4                   可创建会员数量不足
         """
         date_time_now = datetime.datetime.now()
         para['Password'] = cls.sha256_generator(para['Password'])
@@ -80,6 +81,9 @@ class SmUserAgentService(BaseService):
                 return 1
             if agent_user.AgentLevel >= 4:    # 不允许低于4级的用户
                 return 2
+            if agent_user.MemberMaximum < agent_user.MemberNum + para['MemberMaximum']:    # 可用数量不足
+                return 4
+            agent_user.MemberNum += para['MemberMaximum']
             cls.add_agent_to_db(CreatorID=agent_user.ID, MemberNum=0, AgentLevel=agent_user.AgentLevel + 1, CreateTime=date_time_now, RoleID=role['ID'], Forbidden=0, Lock=0, **para)
             cls.create_log(agent_user.ID, role['Description'], '创建代理', date_time_now, '代理' + agent_user.LoginName + '创建次级代理' + para['LoginName'])
         except Exception as e:
@@ -88,9 +92,9 @@ class SmUserAgentService(BaseService):
         return 0
 
     @classmethod
-    def query_agent(cls, agent_id=None, Page=None, PageSize=None):
+    def admin_query_agent(cls, Page=None, PageSize=None):
         """
-        查询所有管理员，按照分页，因无搜索关键字，故不作匹配，同时不查询管理员创建者的名字
+        管理员查询所有代理，按照分页，因无搜索关键字，故不作匹配，同时不查询管理员创建者的名字
         :param Page: 第几页
         :param PageSize: 每页的数量
         :return: 代码    返回结果            阐述
@@ -101,17 +105,137 @@ class SmUserAgentService(BaseService):
             Page = 1
             PageSize = 1000
         try:
-            # 返回分页结果  items当前页结果 total数量
             user_t1 = aliased(SmUser)
-            search_sql = and_()
-            if agent_id:
-                search_sql = and_(SmUserAgent.CreatorID == agent_id)
-            query_sql = [SmUserAgent, user_t1.LoginName.label('CreatorName')]
-            page_result = db.session.query(*query_sql).outerjoin(user_t1, user_t1.ID == SmUserAgent.CreatorID).order_by(SmUserAgent.CreateTime.desc()).filter(search_sql).paginate(Page, PageSize)
+            # 返回分页结果  items当前页结果 total数量
+            page_result = db.session.query(SmUserAgent, user_t1.LoginName.label('CreatorName'))\
+                .outerjoin(user_t1, user_t1.ID == SmUserAgent.CreatorID).order_by(SmUserAgent.CreateTime.desc())\
+                .filter().paginate(Page, PageSize)
             return 0, {"total": page_result.total, "rows": cls.result_to_dict(page_result.items)}
         except Exception as e:
             current_app.logger.error(e)
             return 1, None
+
+    @classmethod
+    def agent_query_agent(cls, agent_user, Page=None, PageSize=None):
+        """
+        代理查询代理(
+            只能是其创建的次级代理
+            即，只能查儿子，不能查询多级代理
+        )，按照分页，因无搜索关键字，故不作匹配，同时不查询管理员创建者的名字
+        :param agent_user: 查询的代理用户
+        :param Page: 第几页
+        :param PageSize: 每页的数量
+        :return: 代码    返回结果            阐述
+                 0       分页查询结果        登陆成功{total, rows}
+                 1       None                其他错误
+        """
+        if Page is None or PageSize is None:
+            Page = 1
+            PageSize = 1000
+        try:
+            user_t1 = aliased(SmUser)
+            # 返回分页结果  items当前页结果 total数量
+            page_result = db.session.query(SmUserAgent, user_t1.LoginName.label('CreatorName')) \
+                .outerjoin(user_t1, user_t1.ID == SmUserAgent.CreatorID).order_by(SmUserAgent.CreateTime.desc()) \
+                .filter(SmUserAgent.CreatorID == agent_user.ID).paginate(Page, PageSize)
+            return 0, {"total": page_result.total, "rows": cls.result_to_dict(page_result.items)}
+        except Exception as e:
+            current_app.logger.error(e)
+            return 1, None
+
+    @classmethod
+    def get_by_id(cls, s_id):
+        """
+        查询代理，通过id，管理员特供
+        :param s_id: 带查询的id
+        :return: 结果
+        """
+        try:
+            user_t1 = aliased(SmUser)
+            result = db.session.query(SmUserAgent, user_t1.LoginName.label('CreatorName'))\
+                .outerjoin(user_t1, user_t1.ID == SmUserAgent.CreatorID)\
+                .filter(SmUserAgent.ID == s_id).first()
+            return cls.result_to_dict(result)
+        except Exception as e:
+            current_app.logger.error(e)
+            return None
+
+    @classmethod
+    def agent_query_by_id(cls, agent_user, s_id):
+        """
+        代理查询次级代理通过id
+        :param agent_user: 父代理用户
+        :param s_id: 子代理用户
+        :return: 查询结果，查到或没有
+        """
+        result = cls.get_by_id(s_id)
+        if not result or result['CreatorID'] != agent_user.ID:
+            return None
+        else:
+            return result
+
+    @classmethod
+    def update_by_id(cls, s_id, **para):
+        """
+        通过id更新代理员
+        :param s_id: 待修改代理的id
+        :param para: 待修改代理的参数
+        :return: 代码    返回结果
+                 0       修改完成
+                 1       登录名重名
+                 2       可使用的会员数不足
+                 3       参数错误
+        """
+        # 校验用户名是否重复
+        try:
+            result = SmUser.query.filter(SmUser.LoginName == para['LoginName']).first()     # 确定是否存在重名
+            if result and result.ID != s_id:
+                return 1
+            target = SmUserAgent.query.filter(SmUserAgent.ID == s_id).first()     # 获取欲修改目标
+            if para['MemberMaximum'] < target.MemberNum:     # 确定是否拥有足够的会员容纳量
+                return 2
+            if target.AgentLevel != 1:     # 确定是否拥有足够的会员容纳量
+                father = SmUserAgent.query.filter(SmUserAgent.ID == target.CreatorID).first()
+                if father.MemberMaximum < father.MemberNum + para['MemberMaximum'] - target.MemberMaximum:
+                    return 2
+                father.MemberNum = father.MemberNum + para['MemberMaximum'] - target.MemberMaximum
+            return super(SmUserAgentService, cls).update_by_id(s_id, **para)
+        except Exception as e:
+            current_app.logger.error(e)
+            return 3
+        return 0
+
+    @classmethod
+    def agent_update_by_id(cls, agent_user, s_id, **para):
+        """
+        通过id更新代理员
+        :param agent_user: 发起操作的代理
+        :param s_id: 待修改代理的id
+        :param para: 待修改代理的参数
+        :return: 代码    返回结果
+                 0       修改完成
+                 1       重名-----登录名
+                 2       权限不足
+                 3       可操作会员数不足
+                 4       提交参数错误
+        """
+        try:
+            result = SmUser.query.filter(SmUser.LoginName == para['LoginName']).first()     # 确定是否存在重名
+            if result and result.ID != s_id:
+                return 1
+            target = SmUserAgent.query.filter(SmUserAgent.ID == s_id).first()     # 获取欲修改目标
+            if target.CreatorID != agent_user.ID:
+                return 2
+            if para['MemberMaximum'] < target.MemberNum:     # 确定是否拥有足够的会员容纳量
+                return 3
+            if agent_user.MemberMaximum < agent_user.MemberNum + para['MemberMaximum'] - target.MemberMaximum:
+                return 3
+            agent_user.MemberNum = agent_user.MemberNum + para['MemberMaximum'] - target.MemberMaximum
+            return super(SmUserAgentService, cls).update_by_id(s_id, **para)
+        except Exception as e:
+            current_app.logger.error(e)
+            return 4
+        return 0
 
     @classmethod
     def change_withdraw_pass(cls, user, old_pass, new_pass):
@@ -132,25 +256,4 @@ class SmUserAgentService(BaseService):
             current_app.logger.error(e)
             return 2
         return 0
-
-    @classmethod
-    def get_by_id(cls, m_id, user_id=None):
-        """
-        查询代理，通过id
-        :param m_id: id
-        :param user_id: 查询者的id
-        :return: 结果
-        """
-        try:
-            search_sql = and_(SmUserAgent.ID == m_id) if not user_id else and_(SmUserAgent.ID == m_id, SmUserAgent.CreatorID == user_id)
-            user_t1 = aliased(SmUser)
-            result = db.session.query(
-                SmUserAgent, user_t1.LoginName.label('CreatorName')).outerjoin(
-                user_t1, user_t1.ID == SmUserAgent.CreatorID).filter(search_sql).first()
-            if len(result) == 0:
-                return None
-            return cls.result_to_dict(result)
-        except Exception as e:
-            current_app.logger.error(e)
-            return None
 
