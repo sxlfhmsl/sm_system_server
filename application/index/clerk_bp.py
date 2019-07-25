811be84bc4a038234608a9163e5cf08f12e23ccf
ShengLu <sxlfhmsl@hotmail.com>
2019-07-25 22:05:31 +0800

    业务员创建

--- application/index/clerk_bp.py
+++ application/index/clerk_bp.py
@@ -2,7 +2,46 @@
 """
 业务员接口， 查询等。。。
 """
+from flask import request, current_app, jsonify
 from flask.blueprints import Blueprint
 
+from .utils import PermissionView
+from ..service.clerk_service import SmClerkService
+from .sta_code import POST_PARA_ERROR, SUCCESS, OTHER_ERROR
 
 clerk_bp = Blueprint('clerk', __name__)
+
+
+class ClerkCreateView(PermissionView):
+    """
+    创建业务员
+    """
+
+    para_legal_list_recv = ['AgentID', 'NickName']
+
+    def response_admin(self):
+        try:
+            result = SmClerkService.create_clerk(self.user, '管理员', **self.unpack_para(request.json))
+            if result == 0:
+                return jsonify(SUCCESS())
+            else:
+                return jsonify(OTHER_ERROR)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(POST_PARA_ERROR)
+
+    def response_agent(self):
+        try:
+            para = self.unpack_para(request.json)
+            para['AgentID'] = self.u_id
+            result = SmClerkService.create_clerk(self.user, '代理', **para)
+            if result == 0:
+                return jsonify(SUCCESS())
+            else:
+                return jsonify(OTHER_ERROR)
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(POST_PARA_ERROR)
+
+
+clerk_bp.add_url_rule('/create', methods=['POST'], view_func=ClerkCreateView.as_view('clerk_create'))
--- application/service/clerk_service.py
+++ application/service/clerk_service.py
@@ -2,12 +2,40 @@
 """
 业务员相关service
 """
+from datetime import datetime
+from flask import current_app
+
 from .utils import BaseService
+from ..dao.models import db, SmClerk
 
 
 class SmClerkService(BaseService):
     """
     业务员相关逻辑代码
     """
-    pass
+
+    @classmethod
+    def create_clerk(cls, user, user_type, **para):
+        """
+        创建业务员
+        :param user: 发起用户
+        :param user_type: 发起用户类型
+        :param para: 参数
+        :return: 执行结果
+        """
+        date_time_now = datetime.now()    # 获取当前时间
+        session = db.session
+        try:
+            if SmClerk.query.filter(SmClerk.NickName == para['NickName']).first():
+                return 1
+            clerk = SmClerk(ID=cls.md5_generator('sm_clerk' + str(date_time_now)), AgentID=para['AgentID'],
+                            NickName=para['NickName'], Forbidden=0)
+            cls.create_log(user.ID, user_type, '创建业务员', date_time_now, user_type + user.LoginName + '创建业务员' + para['NickName'])
+            session.add(clerk)
+            session.commit()
+            return 0
+        except Exception as e:
+            session.rollback()
+            current_app.logger.error(e)
+            return 1
 
